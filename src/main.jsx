21f763f278404538d79ff5bb6d21a728a071afcd
Bryan Schneller <bryan.schneller@gmail.com>
2024-04-18 18:38:36 -0400

    Updates

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <React.StrictMode>
-      <ChakraProvider>
+      <ChakraProvider disableGlobalStyle={true}>
         <App />
       </ChakraProvider>
     </React.StrictMode>,
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,9 +10,22 @@ import {
 } from '@chakra-ui/react'
 
 export default function Contact() {
+    const [nameInput, setNameInput] = useState('');
     const [emailInput, setEmailInput] = useState('');
-    const handleEmailInputChange = (e) => setEmailInput(e.target.value);
-    const isError = emailInput.match('/^[a-zA-Z0-9. _%+-]+@[a-zA-Z0-9. -]+//. [a-zA-Z]{2,}$/');
+    const [textInput, setTextInput] = useState('');
+ 
+    const handleNameInputChange = (e) => {
+        setNameInput(e.target.value);
+    }
+    const handleEmailInputChange = (e) => {
+        setEmailInput(e.target.value);
+    }
+    const handleTextInputChange = (e) => {
+        setTextInput(e.target.value);
+    }
+
+    const isError = emailInput.match(['\A[A-Z0-9+_.-]+@[A-Z0-9.-]+\Z']);
+
 
     return (
         <>
@@ -20,18 +33,21 @@ export default function Contact() {
             <div id="contact-content" className="page-content">
                 <FormControl>
                     <FormLabel>Name</FormLabel>
-                    <Input type="text"></Input>
-                    <FormLabel>Email</FormLabel>
-                    <Input type='email' isInvalid={isError} value={emailInput} onChange={handleEmailInputChange} />
-                    {isError ? (
-                        <FormHelperText>
-                            Enter your email and I will respond as soon as possible.
-                        </FormHelperText>
-                    ) : (
-                        <FormErrorMessage>Email is required.</FormErrorMessage>
-                    )}
+                    <Input type="text" value={nameInput} onChange={handleNameInputChange}></Input>
+                    <FormControl isRequired isInvalid={isError}>
+                        <FormLabel>Email</FormLabel>
+                        <Input type='email' value={emailInput} onChange={handleEmailInputChange}/>
+                        {!isError ? (
+                            <FormHelperText>
+                                Enter the email you'd like to receive the newsletter on.
+                            </FormHelperText>
+                        ) : (
+                            <FormErrorMessage>Email is required.</FormErrorMessage>
+                        )}
+                    </FormControl>
                     <FormLabel>Your Message:</FormLabel>
-                    <Textarea></Textarea>
+                    <Textarea value={textInput} onChange={handleTextInputChange}></Textarea>
+
                     <Button colorScheme='blue'>Submit</Button>
                 </FormControl>
             </div>
