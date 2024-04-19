ab1029193318b19ca3f2053ee2d0c8b3dcae8fb0
Bryan Schneller <bryan.schneller@gmail.com>
2024-04-19 10:36:29 -0400

    Fix contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,22 +9,63 @@ import {
     Button
 } from '@chakra-ui/react'
 
+const EmailInput = () => {
+    const [email, setEmail] = useState('');
+    const [validEmail, setValidEmail] = useState(true);
+
+    const validateEmail = () => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const isValid = emailRegex.test(email);
+        setValidEmail(isValid);
+    };
+
+    const handleFocus = () => {
+        setValidEmail(true);
+    }
+
+    return (
+        <div>
+            <label htmlFor="email">Email:</label>
+            <Input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                onBlur={validateEmail}
+                onFocus={handleFocus}
+            />
+            {!validEmail && <p style={{ color: 'red' }}>Please enter a valid email</p>}
+        </div>
+    );
+};
+
 export default function Contact() {
     const [nameInput, setNameInput] = useState('');
-    const [emailInput, setEmailInput] = useState('');
     const [textInput, setTextInput] = useState('');
- 
+    const [validName, setValidName] = useState(true);
+    const [validText, setValidText] = useState(true);
+
     const handleNameInputChange = (e) => {
         setNameInput(e.target.value);
-    }
-    const handleEmailInputChange = (e) => {
-        setEmailInput(e.target.value);
     }
     const handleTextInputChange = (e) => {
         setTextInput(e.target.value);
     }
-
-    const isError = emailInput.match(['\A[A-Z0-9+_.-]+@[A-Z0-9.-]+\Z']);
+    const handleNameBlur = () => {
+        const isValid = nameInput !== '';
+        setValidName(isValid);
+    }
+    const handleTextBlur = () => {
+        const isValid = textInput !== '';
+        setValidText(isValid);
+    }
+    const handleNameFocus = () => {
+        setValidName(true);
+    }
+    const handleTextFocus = () => {
+        setValidText(true);
+    }
 
 
     return (
@@ -33,20 +74,21 @@ export default function Contact() {
             <div id="contact-content" className="page-content">
                 <FormControl>
                     <FormLabel>Name</FormLabel>
-                    <Input type="text" value={nameInput} onChange={handleNameInputChange}></Input>
-                    <FormControl isRequired isInvalid={isError}>
-                        <FormLabel>Email</FormLabel>
-                        <Input type='email' value={emailInput} onChange={handleEmailInputChange}/>
-                        {!isError ? (
-                            <FormHelperText>
-                                Enter the email you'd like to receive the newsletter on.
-                            </FormHelperText>
-                        ) : (
-                            <FormErrorMessage>Email is required.</FormErrorMessage>
-                        )}
-                    </FormControl>
+                    <Input type="text" 
+                    value={nameInput} 
+                    onChange={handleNameInputChange} 
+                    onBlur={handleNameBlur}
+                    onFocus={handleNameFocus}></Input>
+                    {!validName && <p style={{ color: 'red' }}>Please enter your name</p>}
+
+                    <EmailInput></EmailInput>
+
                     <FormLabel>Your Message:</FormLabel>
-                    <Textarea value={textInput} onChange={handleTextInputChange}></Textarea>
+                    <Textarea value={textInput} 
+                    onChange={handleTextInputChange} 
+                    onBlur={handleTextBlur}
+                    onFocus={handleTextFocus}></Textarea>
+                    {!validText && <p style={{ color: 'red' }}>Please enter a message</p>}
 
                     <Button colorScheme='blue'>Submit</Button>
                 </FormControl>
