0a6eeec647f7bfe1776a408880b5caf1f849f4cc
Bryan Schneller <bryan.schneller@gmail.com>
2025-01-06 09:22:43 -0500

    Edit text on about me page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,12 +4,12 @@ export default function AboutMe() {
             <div id="about-content"  className="page-content">
                 <img id="bryan" src="\images\LinkedInWeddingSmall.JPG" alt="Picture of Bryan" />
                 <div id="about-text-content" className="page-content">
-                    <h2 className="page-title">Hi, I'm Bryan!</h2>
+                    <h2 className="page-title">Bryan Schneller, BS</h2>
                     <p id="about-text">
-                        I'm a full-stack web developer in the Raleigh-Durham, North Carolina area.
-                        I specialize in coding excellent web content and strategizing with my team to boost web functionality and brand recognition.
-                        I challenge myself to optimize the accessibility of the content I create by continuously learning about the needs of
-                        diverse individuals. Let's take your team to the next level!
+                        I am a web developer in the Raleigh-Durham, North Carolina area with proficiency in front and back end web technologies.
+                        I produce strategically-coded content with attention to issues of scale and code sustainability. 
+                        I strive to optimize the accessibility of my web content by continuously learning about the needs of
+                        diverse individuals.  I am an inspired developer who brings passion and creativity to a team development environment. 
                     </p>
                 </div>
             </div>
